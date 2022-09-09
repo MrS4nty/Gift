0e8ee8fecbdb5fd2aaacace3bd88e62984bf7c32
Santy Gonzalez 3 <69918140+MrS4nty@users.noreply.github.com>
2022-09-09 15:35:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,158 +1,153 @@
-// Song data
-const songList = [
-    {
-        title: "Mon Amour ♡",
-        file: "Mon-Amour.mp3",
-        cover: "Mon-Amour.jpg"
-
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      audio: null,
+      circleLeft: null,
+      barWidth: null,
+      duration: null,
+      currentTime: null,
+      isTimerPlaying: false,
+      tracks: [
+        {
+          name: "Everybody Knows",
+          artist: "Leonard Cohen",
+          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
+          url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
+          favorited: true
+        },
+        {
+          name: "Extreme Ways",
+          artist: "Moby",
+          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
+          url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
+          favorited: false
+        },
+        {
+          name: "Butterflies",
+          artist: "Sia",
+          cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
+          source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
+          url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
+          favorited: false
+        } 
+      ],
+      currentTrack: null,
+      currentTrackIndex: 0,
+      transitionName: null
+    };
+  },
+  methods: {
+    play() {
+      if (this.audio.paused) {
+        this.audio.play();
+        this.isTimerPlaying = true;
+      } else {
+        this.audio.pause();
+        this.isTimerPlaying = false;
+      }
     },
-    {
-        title: "Snowman ♡",
-        file: "Snowman.mp3",
-        cover: "snowman.jpg"
+    generateTime() {
+      let width = (100 / this.audio.duration) * this.audio.currentTime;
+      this.barWidth = width + "%";
+      this.circleLeft = width + "%";
+      let durmin = Math.floor(this.audio.duration / 60);
+      let dursec = Math.floor(this.audio.duration - durmin * 60);
+      let curmin = Math.floor(this.audio.currentTime / 60);
+      let cursec = Math.floor(this.audio.currentTime - curmin * 60);
+      if (durmin < 10) {
+        durmin = "0" + durmin;
+      }
+      if (dursec < 10) {
+        dursec = "0" + dursec;
+      }
+      if (curmin < 10) {
+        curmin = "0" + curmin;
+      }
+      if (cursec < 10) {
+        cursec = "0" + cursec;
+      }
+      this.duration = durmin + ":" + dursec;
+      this.currentTime = curmin + ":" + cursec;
     },
-]
-
-// Canción actual
-let actualSong = null
-
-// Capturar elementos del DOM para trabajar con JS
-const songs = document.getElementById("songs")
-const audio = document.getElementById("audio")
-const cover = document.getElementById("cover")
-const title = document.getElementById("title")
-const play = document.getElementById("play")
-const prev = document.getElementById("prev")
-const next = document.getElementById("next")
-const progress = document.getElementById("progress")
-const progressContainer = document.getElementById("progress-container")
-progressContainer.addEventListener("click", setProgress)
-
-// Escuchar el elemento AUDIO
-audio.addEventListener("timeupdate", updateProgress)
-
-// Escuchar clicks en los controles
-play.addEventListener("click", () => {
-    if (audio.paused) {
-        playSong()   
-    } else {
-        pauseSong()
+    updateBar(x) {
+      let progress = this.$refs.progress;
+      let maxduration = this.audio.duration;
+      let position = x - progress.offsetLeft;
+      let percentage = (100 * position) / progress.offsetWidth;
+      if (percentage > 100) {
+        percentage = 100;
+      }
+      if (percentage < 0) {
+        percentage = 0;
+      }
+      this.barWidth = percentage + "%";
+      this.circleLeft = percentage + "%";
+      this.audio.currentTime = (maxduration * percentage) / 100;
+      this.audio.play();
+    },
+    clickProgress(e) {
+      this.isTimerPlaying = true;
+      this.audio.pause();
+      this.updateBar(e.pageX);
+    },
+    prevTrack() {
+      this.transitionName = "scale-in";
+      this.isShowCover = false;
+      if (this.currentTrackIndex > 0) {
+        this.currentTrackIndex--;
+      } else {
+        this.currentTrackIndex = this.tracks.length - 1;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    nextTrack() {
+      this.transitionName = "scale-out";
+      this.isShowCover = false;
+      if (this.currentTrackIndex < this.tracks.length - 1) {
+        this.currentTrackIndex++;
+      } else {
+        this.currentTrackIndex = 0;
+      }
+      this.currentTrack = this.tracks[this.currentTrackIndex];
+      this.resetPlayer();
+    },
+    resetPlayer() {
+      this.barWidth = 0;
+      this.circleLeft = 0;
+      this.audio.currentTime = 0;
+      this.audio.src = this.currentTrack.source;
+      setTimeout(() => {
+        if(this.isTimerPlaying) {
+          this.audio.play();
+        } else {
+          this.audio.pause();
+        }
+      }, 300);
+    },
+    favorite() {
+      this.tracks[this.currentTrackIndex].favorited = !this.tracks[
+        this.currentTrackIndex
+      ].favorited;
     }
-})
-
-next.addEventListener("click", () => nextSong())
-prev.addEventListener("click", () => prevSong())
-
-// Cargar canciones y mostrar el listado
-function loadSongs() {
-    songList.forEach((song, index) => {
-        // Crear li
-        const li = document.createElement("li")
-        // Crear a
-        const link = document.createElement("a")
-        // Hidratar a
-        link.textContent = song.title
-        link.href = "#"
-        // Escuchar clicks
-        link.addEventListener("click", () => loadSong(index))
-        // Añadir a li
-        li.appendChild(link)
-        // Aañadir li a ul
-        songs.appendChild(li)
-    })
-}
-
-// Cargar canción seleccionada
-function loadSong(songIndex) {
-    if (songIndex !== actualSong) {
-        changeActiveClass(actualSong, songIndex)
-        actualSong = songIndex
-        audio.src = "./audio/" + songList[songIndex].file
-        playSong()
-        changeSongtitle(songIndex)
-        changeCover(songIndex)
-    }
-}
-
-// Actualizar barra de progreso de la canción
-function updateProgress(event) {
-    const {duration, currentTime} = event.srcElement
-    const percent = (currentTime / duration) * 100
-    progress.style.width = percent + "%" 
-}
-
-// Hacer la barra de progreso clicable
-function setProgress(event) {
-    const totalWidth = this.offsetWidth
-    const progressWidth = event.offsetX
-    const current = (progressWidth / totalWidth) * audio.duration
-    audio.currentTime = current
-}
-
-// Actualiar controles
-function updateControls() {
-    if (audio.paused) {
-        play.classList.remove("fa-pause")
-        play.classList.add("fa-play")
-    } else {
-        play.classList.add("fa-pause")
-        play.classList.remove("fa-play")
-    }
-}
-
-// Reproducir canción
-function playSong() {
-    if (actualSong !== null) {
-        audio.play()
-        updateControls()
-    }
-}
-
-// Pausar canción
-function pauseSong() {
-    audio.pause()
-    updateControls()
-}
-
-// Cambiar clase activa
-function changeActiveClass(lastIndex, newIndex) {
-    const links = document.querySelectorAll("a")
-    if (lastIndex !== null) {
-        links[lastIndex].classList.remove("active")
-    }
-    links[newIndex].classList.add("active")
-}
-
-// Cambiar el cover de la canción
-function changeCover(songIndex) {
-    cover.src = "./img/" + songList[songIndex].cover
-}
-
-// Cambiar el título de la canción
-function changeSongtitle(songIndex) {
-     title.innerText = songList[songIndex].title
-}
-
-// Anterior canción
-function prevSong() {
-    if (actualSong > 0) {
-        loadSong(actualSong - 1)
-    } else {
-        loadSong(songList.length - 1)
-    }
-}
-
-// Siguiente canción
-function nextSong() {
-    if (actualSong < songList.length -1) {
-        loadSong(actualSong + 1)
-    } else {
-        loadSong(0)
-    }
-}
-
-// Lanzar siguiente canción cuando se acaba la actual
-audio.addEventListener("ended", () => nextSong())
-
-// GO!
-loadSongs()
+  },
+  created() {
+    let vm = this;
+    this.currentTrack = this.tracks[0];
+    this.audio = new Audio();
+    this.audio.src = this.currentTrack.source;
+    this.audio.ontimeupdate = function() {
+      vm.generateTime();
+    };
+    this.audio.onloadedmetadata = function() {
+      vm.generateTime();
+    };
+    this.audio.onended = function() {
+      vm.nextTrack();
+      this.isTimerPlaying = true;
+    };
+  }
+});
