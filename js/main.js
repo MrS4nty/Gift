ae288036476f097fa8a59ffb8eb1616a23638604
Santy Gonzalez 3 <69918140+MrS4nty@users.noreply.github.com>
2022-09-09 17:22:47 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,10 +12,10 @@ new Vue({
         {
           name: "My Snowman <3",
           artist: "Sia X Amoshito :3",
-          cover: "Mon-Amour.jpg",
-          source: "Mon-Amour.mp3",
+          cover: "https://raw.githubusercontent.com/MrS4nty/Gift/gh-pages/img/Snowman.jpg",
+          source: "https://raw.githubusercontent.com/MrS4nty/Gift/gh-pages/audio/Snowman.mp3",
           url: "https://instagram.com/mrs4nty",
-          favorited: true
+          favorited: false
         },
         {
           name: "Extreme Ways",
