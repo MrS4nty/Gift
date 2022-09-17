b7b48d1ca176c9d7b098e0665cbfe1ea61425b88
Santy Gonzalez 3 <69918140+MrS4nty@users.noreply.github.com>
2022-09-16 21:46:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let scrollBar = document.getElementById('scrollbar')
 let height = document.body.scrollHeight - window.innerHeight
 window.onscroll = function (){
-        let scrollHeight = (window.pageYOffset / height) * 1000
+        let scrollHeight = (window.pageYOffset / height) * 500
         scrollBar.style.height = `${scrollHeight}%`
 }
