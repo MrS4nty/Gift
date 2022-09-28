7815096115a8629cf816e07b17b740f01e6067d9
Santy Gonzalez 3 <69918140+MrS4nty@users.noreply.github.com>
2022-09-28 18:52:50 -0500

    Create main3.js

---
+++ main3.js
@@ -0,0 +1,37 @@
+(function() {
+	var $body = document.body
+	, $menu_trigger = $body.getElementsByClassName('menu-trigger')[0];
+
+	if ( typeof $menu_trigger !== 'undefined' ) {
+		$menu_trigger.addEventListener('click', function() {
+			$body.className = ( $body.className == 'menu-active' )? '' : 'menu-active';
+		});
+	}
+
+}).call(this);
+
+// SubMenus
+
+const mostrar1 = () => {
+  if(document.querySelector(".submenucontent").classList.contains("ocultar")){
+     document.querySelector(".submenucontent").classList.remove("ocultar");
+  } else {
+     document.querySelector(".submenucontent").classList.add("ocultar");
+  }
+}
+
+const mostrar2 = () => {
+  if(document.querySelector(".submenucontent2").classList.contains("ocultar")){
+     document.querySelector(".submenucontent2").classList.remove("ocultar");
+  } else {
+     document.querySelector(".submenucontent2").classList.add("ocultar");
+  }
+}
+
+const mostrar3 = () => {
+  if(document.querySelector(".submenucontent3").classList.contains("ocultar")){
+     document.querySelector(".submenucontent3").classList.remove("ocultar");
+  } else {
+     document.querySelector(".submenucontent3").classList.add("ocultar");
+  }
+}
